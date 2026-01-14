71a693bc803ec342a02c3992f0c51d42a59bf509
Roberto Gama <gamaroberto-sketch@users.noreply.github.com>
2026-01-14 19:29:54 -0300

    chore: force favicon cache refresh with v3 query param

--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,8 @@ export default function Document() {
     <Html lang="pt-BR">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/favicon.png" />
-        <link rel="shortcut icon" href="/favicon.png" />
+        <link rel="icon" href="/favicon.png?v=3" type="image/png" />
+        <link rel="shortcut icon" href="/favicon.ico?v=3" sizes="any" />
       </Head>
       <body className="antialiased">
         <Main />
