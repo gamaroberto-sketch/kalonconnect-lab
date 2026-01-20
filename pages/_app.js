062bd9e4f9171def801a7f91328128238b918e5e
Roberto Gama <gamaroberto-sketch@users.noreply.github.com>
2026-01-20 15:49:41 -0300

    fix(critical): restore imports in _app.js broken by previous commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import GlobalErrorBoundary from '../components/GlobalErrorBoundary'; // 🟢 Added Import
+import "@/styles/globals.css";
+import '../styles/autofill-fix.css';
+import "@livekit/components-styles";
+import "@livekit/components-styles/prefabs/index.css";
+import { AuthProvider } from '../components/AuthContext';
+import { ThemeProvider } from '../components/ThemeProvider';
+import { LanguageProvider } from '../contexts/LanguageContext';
+import { FeedbackProvider } from '../contexts/FeedbackContext';
+import AdminAccess from '../components/AdminAccess';
+import ProfessionalGuideGate from '../components/ProfessionalGuideGate';
+import { useEffect } from 'react';
+import GlobalErrorBoundary from '../components/GlobalErrorBoundary';
 
 export default function App({ Component, pageProps }) {
-  // ... (useEffect remains same)
+  useEffect(() => {
+    // Remover service workers e caches
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.getRegistrations().then((regs) => {
+        regs.forEach((reg) => reg.unregister());
+      });
+    }
+
+    if ("caches" in window) {
+      caches.keys().then((keys) => {
+        keys.forEach((key) => caches.delete(key));
+      });
+    }
+  }, []);
 
   return (
     <ThemeProvider>
