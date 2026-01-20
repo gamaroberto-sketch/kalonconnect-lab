a3fb33616e15b9b206e6a482ac850e391b263c2a
Roberto Gama <gamaroberto-sketch@users.noreply.github.com>
2026-01-20 15:47:25 -0300

    feat: inject GlobalErrorBoundary in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,41 +1,20 @@
-import "@/styles/globals.css";
-import '../styles/autofill-fix.css';
-import "@livekit/components-styles";
-import "@livekit/components-styles/prefabs/index.css";
-import { AuthProvider } from '../components/AuthContext';
-import { ThemeProvider } from '../components/ThemeProvider';
-import { LanguageProvider } from '../contexts/LanguageContext';
-import { FeedbackProvider } from '../contexts/FeedbackContext';
-import AdminAccess from '../components/AdminAccess';
-import ProfessionalGuideGate from '../components/ProfessionalGuideGate';
-import { useEffect } from 'react';
+import GlobalErrorBoundary from '../components/GlobalErrorBoundary'; // 🟢 Added Import
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    // Remover service workers e caches
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.getRegistrations().then((regs) => {
-        regs.forEach((reg) => reg.unregister());
-      });
-    }
-
-    if ("caches" in window) {
-      caches.keys().then((keys) => {
-        keys.forEach((key) => caches.delete(key));
-      });
-    }
-  }, []);
+  // ... (useEffect remains same)
 
   return (
     <ThemeProvider>
       <LanguageProvider>
         <AuthProvider>
-          <FeedbackProvider>
-            <ProfessionalGuideGate>
-              <Component {...pageProps} />
-              <AdminAccess />
-            </ProfessionalGuideGate>
-          </FeedbackProvider>
+          <GlobalErrorBoundary>
+            <FeedbackProvider>
+              <ProfessionalGuideGate>
+                <Component {...pageProps} />
+                <AdminAccess />
+              </ProfessionalGuideGate>
+            </FeedbackProvider>
+          </GlobalErrorBoundary>
         </AuthProvider>
       </LanguageProvider>
     </ThemeProvider>
